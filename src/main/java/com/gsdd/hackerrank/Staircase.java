7f0cae558b5ce2b4401e7ebbe49d345d77f85de3
Alex Galvis <alex.galvis.sistemas@gmail.com>
2021-12-13 12:21:55 -0500

    Update project structure

---
+++ src/main/java/com/gsdd/hackerrank/Staircase.java
@@ -0,0 +1,26 @@
+package com.gsdd.hackerrank;
+
+import java.util.Scanner;
+
+public class Staircase {
+
+  // Complete the staircase function below.
+  static void staircase(int n) {
+    for (int i = 1; i <= n; i++) {
+      StringBuilder sb = new StringBuilder(n);
+      System.out.println(sb.replace(n - 2, n - 1, "#"));
+    }
+
+  }
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    int n = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+    staircase(n);
+
+    scanner.close();
+  }
+}
